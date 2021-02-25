09f0c1aa2c7263163c00c2c5cdd889ea37962a94
Yukari316 <cbgancna@gmail.com>
2021-02-25 22:30:57 +0800

    添加没什么用的测试程序（

---
+++ ToolBoxTest/Program.cs
@@ -0,0 +1,12 @@
+using YukariToolBox.FormatLog;
+
+namespace ToolBoxTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Info("wow","wow");
+        }
+    }
+}
