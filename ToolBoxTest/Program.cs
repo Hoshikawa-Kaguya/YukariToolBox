baefe67c0c4e96ce753a32b00b7dc2fea6000140
小何 <xiaohe321@outlook.com>
2021-03-13 01:40:08 +0800

    新建LINQ 更新扩展

--- ToolBoxTest/Program.cs
+++ ToolBoxTest/Program.cs
@@ -1,17 +1,32 @@
+using System;
+using System.Collections.Generic;
+using YukariToolBox.Extensions;
 using YukariToolBox.FormatLog;
 
 namespace ToolBoxTest
 {
     class Program
     {
+        class person
+        {
+            public int    id;
+            public string a = "";
+        }
+
         static void Main(string[] args)
         {
             Log.SetLogLevel(LogLevel.Debug);
-            Log.Debug("wow","wow");
-            Log.Info("wow","wow");
-            Log.Fatal("wow","wow");
-            Log.Warning("wow","wow");
-            Log.Error("wow","wow");
+            Log.Debug("wow", "wow");
+            Log.Info("wow", "wow");
+            Log.Fatal("wow", "wow");
+            Log.Warning("wow", "wow");
+            Log.Error("wow", "wow");
+
+            var list = new List<int>() {1, 2, 3, 4, 5};
+            list.WhereUpdate(i => i !=2)
+                .Update(1);
+                 
+            Console.ReadKey();
         }
     }
 }
---
+++ YukariToolBox/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace YukariToolBox.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static bool Update<TSource>(this IEnumerable<TSource> source, Action<TSource> updateAction)
+            where TSource : class
+        {
+            foreach (TSource item in source)
+            {
+                updateAction(item);
+            }
+
+            return true;
+        }
+
+        public static Tuple<IEnumerable<TSource>, IList<TSource>> WhereUpdate<TSource>(
+            this IList<TSource> source, Func<TSource, bool> whereAction) where TSource : struct
+        {
+            return new(source.Where(whereAction), source);
+        }
+
+        public static Tuple<IEnumerable<TSource>, IList<TSource>> Update<TSource>(
+            this Tuple<IEnumerable<TSource>, IList<TSource>> source,
+            TSource newValue)
+        {
+            var temp = source.Item1.ToList();
+            for (var i = 0; i < temp.Count; i++)
+            {
+                int index = source.Item2.ToList().FindIndex(j => j.Equals(temp[i]));
+                source.Item2[index] = newValue;
+            }
+
+            //source.Item2 = temp;
+            return new (source.Item1, source.Item2);
+        }
+    }
+}
